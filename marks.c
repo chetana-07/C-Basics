adc960da4b7a1e2c272eaed364c6d66fb7d135b2
chetana-07 <92291269+chetana-07@users.noreply.github.com>
2021-10-25 11:42:50 +0530

    Add files via upload

---
+++ marks.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int p,c,m,e,total;
+	p=50;
+	c=60;
+	m=70;
+	e=40;
+	total=p+c+m+e;
+	printf("marks in physics:%d\n",p);
+	printf("marks in chemistry:%d\n",c);
+	printf("marks in maths:%d\n",m);
+	printf("marks in english:%d\n",e);
+	printf("total marks:%d",total);
+		
+}
