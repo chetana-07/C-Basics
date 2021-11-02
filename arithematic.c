2f38d40e470034cf4c72dbdf25dd84c697b2b57a
chetana-07 <92291269+chetana-07@users.noreply.github.com>
2021-11-02 11:37:21 +0530

    Add files via upload

---
+++ arithematic.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,s,c,p,d,m;
+	a=11;
+	b=2;
+	s=a+b;
+	printf("sum of %d and %d is %d:\n",a,b,s);
+	c=a-b;
+	printf("difference of %d and %d is %d:\n",a,b,c);
+	p=a*b;
+	printf("product of %d and %d is %d:\n",a,b,p);
+	d=a/b;
+	printf("quotient of %d and %d is %d:\n",a,b,d);
+	m=a%b;
+	printf("remainder of %d and %d is %d:",a,b,m);
+	
+}
