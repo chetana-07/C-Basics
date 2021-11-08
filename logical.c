95b6e1acdf9a9480443cf670e5f738f8acd24c7b
chetana-07 <92291269+chetana-07@users.noreply.github.com>
2021-11-08 12:27:29 +0530

    Add files via upload

---
+++ logical.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	printf("%d\n",(20>10) && (30>20));
+	printf("%d\n",(30>5) && (40<5));
+	printf("%d\n",(20>1) || (3<1));
+	printf("%d\n",(20<10)|| (10<5));
+	
+	
+}
