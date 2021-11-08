a3b4ff5015acada456229fa060d89d9a634ba84e
chetana-07 <92291269+chetana-07@users.noreply.github.com>
2021-11-08 12:27:11 +0530

    Add files via upload

---
+++ comparision.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("enter a number");
+	scanf("%d",&a);
+	printf("enter a number");
+	scanf("%d",&b);
+	printf("%d > %d--->%d\n",a,b,a>b);
+	printf("%d < %d--->%d\n",a,b,a<b);
+	printf("%d >= %d--->%d\n",a,b,a>=b);
+	printf("%d <= %d--->%d\n",a,b,a<=b);
+	printf("%d == %d--->%d\n",a,b,a==b);
+	printf("%d != %d--->%d\n",a,b,a!=b);
+}
