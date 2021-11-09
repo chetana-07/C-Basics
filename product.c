dd027464bfa0aea3aba1fc7c8a8e6be3ffe50b88
chetana-07 <92291269+chetana-07@users.noreply.github.com>
2021-11-09 12:28:28 +0530

    Add files via upload

---
+++ product.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d,product;
+	printf("enter four numbers");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	product=a*b*c*d;
+	printf("product of %d,%d,%d and %d is:%d",a,b,c,d,product);
+	
+}
