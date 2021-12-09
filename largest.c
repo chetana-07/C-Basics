756e63cc24c81e5a735a268ca749b550d321c12b
chetana-07 <92291269+chetana-07@users.noreply.github.com>
2021-12-09 22:11:50 +0530

    Add files via upload

---
+++ largest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	if(a>b&&a>c&&b>d)
+	{
+		printf("%d is the largest",a);
+	}
+	else if(b>a&&b>c&&b>d)
+	{
+		printf("%d is the largest",b);
+	}
+	else if(c>a&&c>b&&c>d)
+	{
+		printf("%d is the largest",c);
+	}
+	else
+	{
+		printf("%d is the largest",d);
+	}
+}
