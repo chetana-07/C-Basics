cdfd84633c5c09334a1dd85f3e4c66dd043d81bd
chetana-07 <92291269+chetana-07@users.noreply.github.com>
2021-12-09 22:11:19 +0530

    Add files via upload

---
+++ greater num.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+		printf("%d is greater ",a);	
+	}
+	else if(b>a)
+	{
+		printf("%d is greater",b);
+	}
+	else
+	{
+		printf("%d and %d are equal",a,b);
+	}
+}
